b6d31b7e6aa8c6870aaa6cf47cffb37e1c964c58
yoavben <yoav@tikalk.com>
2019-02-18 18:48:13 +0200

    step32

---
+++ src/main/java/com/tikal/springcloud/netflixzuulapigatewayserver/ZuulLoggingFilter.java
@@ -0,0 +1,36 @@
+package com.tikal.springcloud.netflixzuulapigatewayserver;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class ZuulLoggingFilter extends ZuulFilter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ZuulLoggingFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+        LOGGER.info("request={}",request);
+        return null;
+    }
+}
